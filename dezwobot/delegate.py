4b058b9d6d60cfcbd6b5c67d26fce674324e7596
ijiliji <ijiliji@users.noreply.github.com>
2022-07-21 11:54:48 +0200

    Add submission types

--- dezwobot/delegate.py
+++ dezwobot/delegate.py
@@ -31,12 +31,14 @@ submission_types = {
     "tagesschau.de":        SubmissionType.no_paywall,
     "wdr.de":               SubmissionType.no_paywall,
     "zdf.de":               SubmissionType.no_paywall,
+    "funk.net":             SubmissionType.no_paywall,
 
     "netzpolitik.org":      SubmissionType.no_paywall,
     "t-online.de":          SubmissionType.no_paywall,
     "taz.de":               SubmissionType.no_paywall,
     "derstandard.at":       SubmissionType.no_paywall,
     "orf.at":               SubmissionType.no_paywall,
+    "theguardian.com":      SubmissionType.no_paywall,
 
     "rnd.de":               SubmissionType.only_static_paywall,
 
